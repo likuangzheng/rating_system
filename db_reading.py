bed70aed8211f346ee65bcad99495eaf24f627e0
likuangzheng <likuangzheng@itgwn.com>
2018-04-02 17:38:37 +0800

    new format

--- db_reading.py
+++ db_reading.py
@@ -6,9 +6,9 @@ import json
 import datetime
 
 def generate_ana_table(jj, seasonId):
-    season = jj['seasons']#[ObjectId('58297abea723c1077adf5d8b')]
+    season_db = jj['seasons']#[ObjectId('58297abea723c1077adf5d8b')]
     seasonId = ObjectId(seasonId)
-    season_info = season.find({"_id":seasonId})#, 'status':'FINISHED'})
+    season_info = list(season_db.find({"_id":seasonId}))#, 'status':'FINISHED'})
     sessions = season_info[0]['sessions']
     #有没有问题啊
     history_db = jj['gamehistories']
@@ -22,28 +22,32 @@ def generate_ana_table(jj, seasonId):
             g_config = g_info['gameConfig']
             startbid = list(jj['gameconfigs'].find({'_id':g_config}))[0]
             startbid = startbid['bidStartPosition']
-
+            
             for p in peoples:
-                #if p['status'] == 'FINISHED':
-                hist_id = p['history']
-                h = history_db.find({"_id" : hist_id})
-                end = json.loads(list(h)[0]['rawInput'])['endAt']
-                end = datetime.datetime.fromtimestamp(end/1000)
-                res = {'seasonId':str(seasonId),
-                       'sessonId':str(ses['_id']),
-                       'gameId':str(g),
-                       'user':p['uid'],
-                       'bid':p['bid'],
-                       'bidStartPosition':startbid,
-                       'bombs':p['bombs'],
-                       'spring':p['spring'],
-                       'gameResult':p['score'],
-                       'hist_id':str(p['history']),
-                       'role':p['role'],
-                       'endTime':end,
-                       'status':p['status']
-                        }
-                table.append(res)
+                if p['status'] == 'FINISHED':
+                    hist_id = p['history']
+                    h = history_db.find({"_id" : hist_id})
+                    end = json.loads(list(h)[0]['rawInput'])['endAt']
+                    end = datetime.datetime.fromtimestamp(end/1000)
+                    user_id = p['uid']
+                    roles = {0:'C', 1:'E', 2:'D', 3:'F'}
+                    user_name = list(jj['users'].find({'_id':ObjectId(p['uid'])}))[0]['name']
+                    res = {'seasonId':str(seasonId),
+                           'sessonId':str(ses['_id']),
+                           'gameId':str(g),
+                           'user_name':user_name,
+                           'user':p['uid'],
+                           'bid':p['bid'],
+                           'bidStartPosition':startbid,
+                           'bombs':p['bombs'],
+                           'spring':p['spring'],
+                           'gameResult':p['score'],
+                           'hist_id':str(p['history']),
+                           'role': roles[p['role']],
+                           'endTime':end,
+                           'status':p['status']
+                            }
+                    table.append(res)
     return DataFrame(table)
 
 if __name__ == '__main__':
